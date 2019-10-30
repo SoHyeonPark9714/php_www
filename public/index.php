a077f8f21d8361ccf17c2f3afc24c138009e1037
SoHyeonPark9714 <at971004er@gmail.com>
2019-10-30 16:04:58 +0900

    오후 수업 연상배열

--- public/index.php
+++ public/index.php
@@ -1,25 +1,21 @@
 <?php
-
 $config = include "../dbconf.php";
-echo "대림대학교";
-print_r($config);
-//같은 경로를 지정해주지 않고 웹사이트상에서 사용자가
-//접속을 하지 못하게 한다.
 require "../Loading.php";
-/*
-require "../Module/Database/Database.php";
-require "../Module/Database/table.php"; 
-*/
-    
-$db = new Database($config);
-$table = new Table($db);
-echo "<br>";
-$query = "SHOW TABLES";
-$result = $db->queryExecute($query);
-
-
-$count = mysqli_num_rows($result);
-for($i=0;$i<$count;$i++){
-    $row = mysqli_fetch_object($result);
-    echo $row->Tables_in_php."<br>";
+$uri = $_SERVER['REQUEST_URI'];
+$uris = explode("/",$uri); // 파란책
+// print_r($uris);
+$db = new \Module\Database\Database( $config );
+if(isset($uris[1]) && $uris[1]) {
+    // 컨트롤러 실행...
+    // echo $uris[1]."컨트롤러 실행...";
+    $controllerName = "\App\Controller\\" . ucfirst($uris[1]);
+    $tables = new $controllerName ($db);
+    $tables->main();
+} else {
+    // 처음 페이지 에요.
+    // echo "처음 페이지 에요.";
+    $body = file_get_contents("../Resource/index.html");
+    echo $body;
 }
+// $desc = new \App\Controller\TableInfo;
+// $desc->main();
