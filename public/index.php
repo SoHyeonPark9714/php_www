9883122c1e49793f3d7326d11091a7dab106dae6
SoHyeonPark9714 <at971004er@gmail.com>
2019-10-30 11:18:00 +0900

    git_www 추가

--- public/index.php
+++ public/index.php
@@ -1,2 +1,28 @@
 <?php
+
+$config = include "../dbconf.php";
 echo "대림대학교";
+print_r($config);
+//같은 경로를 지정해주지 않고 웹사이트상에서 사용자가
+//접속을 하지 못하게 한다.
+
+require "../Module/Database/Database.php";
+require "../Module/Database/table.php";
+
+
+$config = include "dbconf.php";
+require "database.php";
+require "table.php";
+    
+$db = new Database($config);
+$table = new Table($db);
+echo "<br>";
+$query = "SHOW TABLES";
+$result = $db->queryExecute($query);
+
+
+$count = mysqli_num_rows($result);
+for($i=0;$i<$count;$i++){
+    $row = mysqli_fetch_object($result);
+    echo $row->Tables_in_php."<br>";
+}
